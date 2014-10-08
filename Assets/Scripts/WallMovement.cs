32b00c26ec92b968c2bb5afcd41811958515c15c
Cody Swendrowski <cody@swendrowski.us>
2014-10-07 19:27:42 -0500

    Unity Push
    This contains all of the assets, scenes, and scripts created in ep1 of
    Unity tutorial.

---
+++ Assets/Scripts/WallMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallMovement : MonoBehaviour {
+
+	public Vector2 speed = new Vector2(50,0);
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		rigidbody2D.velocity = speed;
+	}
+}
